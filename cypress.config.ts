9d408ee8ee9a6686c1f206c92ad972b9bdb3d9f1
Andrei <fateeand@gmail.com>
2023-04-18 21:29:02 +0200

    Add Cypress components testing

---
+++ cypress.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: 'projects/cps-ui-kit/**/*.cy.ts',
+    video: false,
+  },
+});
---
+++ projects/cps-ui-kit/src/lib/components/cps-input/cps-input.component.cy.ts
@@ -0,0 +1,7 @@
+import { CpsInputComponent } from './cps-input.component';
+
+describe('CpsIconComponent', () => {
+  it('should mount', () => {
+    cy.mount(CpsInputComponent);
+  });
+});
