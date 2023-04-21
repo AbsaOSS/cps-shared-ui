7f326b1579f91a454fe08c5337a212d0de8a4e13
Andrei Fateev <104006376+fateeand@users.noreply.github.com>
2023-04-21 14:28:39 +0200

    Create checkbox component (#38)

---
+++ projects/cps-ui-kit/src/lib/components/cps-checkbox/cps-checkbox.component.cy.ts
@@ -0,0 +1,7 @@
+import { CpsCheckboxComponent } from './cps-checkbox.component'
+
+describe('CpsCheckboxComponent', () => {
+  it('should mount', () => {
+    cy.mount(CpsCheckboxComponent)
+  })
+})
---
+++ projects/cps-ui-kit/src/lib/components/cps-checkbox/cps-checkbox.component.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Optional,
+  Output,
+  Self,
+} from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'cps-checkbox',
+  templateUrl: './cps-checkbox.component.html',
+  styleUrls: ['./cps-checkbox.component.scss'],
+})
+export class CpsCheckboxComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() disabled = false;
+  @Input() set value(value: boolean) {
+    this._value = value;
+    this.onChange(value);
+  }
+  get value(): boolean {
+    return this._value;
+  }
+
+  @Output() valueChanged = new EventEmitter<boolean>();
+
+  private _value = false;
+
+  constructor(
+    @Self() @Optional() private _control: NgControl,
+    private _elementRef: ElementRef<HTMLElement>
+  ) {
+    if (this._control) {
+      this._control.valueAccessor = this;
+    }
+  }
+
+  onChange = (event: any) => {};
+  onTouched = () => {};
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  writeValue(value: boolean) {
+    this.value = value;
+  }
+
+  updateValueEvent(event: any) {
+    event.preventDefault();
+    if (this.disabled) return;
+    this._updateValue(!this.value);
+  }
+
+  private _updateValue(value: boolean) {
+    this.writeValue(value);
+    this.onChange(value);
+    this.valueChanged.emit(value);
+  }
+
+  setDisabledState(disabled: boolean) {}
+
+  onBlur() {
+    this._control?.control?.markAsTouched();
+  }
+
+  focus() {
+    this._elementRef?.nativeElement?.querySelector('input')?.focus();
+  }
+}
