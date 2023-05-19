86815d8bcb520d206b20a5d60667765fa3c3d538
Andrei <fateeand@gmail.com>
2023-05-19 10:36:46 +0200

    fix eslint err

--- projects/cps-ui-kit/src/lib/components/cps-icon/cps-icon.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-icon/cps-icon.component.ts
@@ -102,7 +102,7 @@ export const iconNames = [
   'wallet',
   'warning',
   'widget-button-icon',
-  'xls',
+  'xls'
 ];
 
 export type IconType = (typeof iconNames)[number];
@@ -121,7 +121,7 @@ export type iconSizeType =
   imports: [CommonModule],
   selector: 'cps-icon',
   templateUrl: './cps-icon.component.html',
-  styleUrls: ['./cps-icon.component.scss'],
+  styleUrls: ['./cps-icon.component.scss']
 })
 export class CpsIconComponent implements OnChanges {
   @Input() icon: IconType = '';
@@ -135,12 +135,12 @@ export class CpsIconComponent implements OnChanges {
 
   classesList: string[] = ['icon'];
 
-  ngOnChanges(): void {
+  ngOnChanges (): void {
     this.iconColor = getCSSColor(this.color);
     this.setClasses();
   }
 
-  setClasses(): void {
+  setClasses (): void {
     switch (this.size) {
       case 'fill': {
         this.classesList.push('icon--fill');
