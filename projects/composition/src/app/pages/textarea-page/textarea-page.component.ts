0c0e2fc1df485194fef38d7015c0823633ce3417
Victor Vargas <vvargas2013@gmail.com>
2023-06-09 11:27:36 +0100

    latest changes

--- projects/composition/src/app/pages/textarea-page/textarea-page.component.ts
+++ projects/composition/src/app/pages/textarea-page/textarea-page.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators
+} from '@angular/forms';
 import { CpsTextareaComponent } from 'cps-ui-kit';
 
 @Component({
   standalone: true,
   selector: 'app-textarea-page',
-  imports: [CpsTextareaComponent],
+  imports: [CpsTextareaComponent, ReactiveFormsModule],
   templateUrl: './textarea-page.component.html',
   styleUrls: ['./textarea-page.component.scss'],
   host: { class: 'composition-page' }
 })
-export class TextareaPageComponent {}
+export class TextareaPageComponent implements OnInit {
+  form!: UntypedFormGroup;
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(private _formBuilder: UntypedFormBuilder) {}
+
+  ngOnInit() {
+    this.form = this._formBuilder.group({
+      requiredTextarea: ['', [Validators.required]]
+    });
+  }
+}
