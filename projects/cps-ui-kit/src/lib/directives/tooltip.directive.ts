a77e291d4f824db8f51fbe861d6b632b52a7d8a1
Victor Vargas <vvargas2013@gmail.com>
2023-06-19 18:07:04 +0100

    implemented auto repositioning if not enough space

--- projects/cps-ui-kit/src/lib/directives/tooltip.directive.ts
+++ projects/cps-ui-kit/src/lib/directives/tooltip.directive.ts
@@ -9,33 +9,76 @@ export class TooltipDirective {
   @Input() openDelay: string | number = 300;
   @Input() closeDelay: string | number = 300;
   @Input() autoClose = true;
-  @Input() closeOnContentClick = false;
   @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
   @Input() disabled = false;
   @Input() openOn: 'hover' | 'focus' | 'click' = 'hover';
+  @Input() set closeOnContentClick(value: boolean) {
+    if (!this.autoClose && value) {
+      throw new Error(
+        'closeOnContentClick cannot be true when autoClose is false'
+      );
+    }
+    this.closeOnContentClick = value;
+  }
 
-  private _popup?: HTMLDivElement;
+  get closeOnContentClick() {
+    return this.closeOnContentClick;
+  }
 
-  opened = false;
+  private _popup: HTMLDivElement = document.createElement('div');
+  private _opened = false;
 
   // eslint-disable-next-line no-useless-constructor
   constructor(private _elementRef: ElementRef<HTMLElement>) {}
 
   private _createTooltip = () => {
-    const { x, y } = this._setTooltipPosition();
-    this._popup = document.createElement('div');
+    const tooltip = document.body.appendChild(this._popup);
 
-    this._popup.innerHTML = this.tooltip;
-    this._popup.setAttribute('class', 'cps-tooltip');
-    this._popup.style.top = y.toString() + 'px';
-    this._popup.style.left = x.toString() + 'px';
+    this._constructElement();
 
-    document.body.appendChild(this._popup);
+    if (
+      this._popup &&
+      !this._checkIfEnoughSpace(
+        {
+          x: this._popup.getBoundingClientRect().left,
+          y: this._popup.getBoundingClientRect().top
+        },
+        tooltip
+      )
+    ) {
+      this.position = 'top';
+
+      this._destroyTooltip();
+      this._createTooltip();
+    }
   };
 
   private _destroyTooltip = () => {
     this._popup?.remove();
   };
+
+  private _constructElement() {
+    this._popup.innerHTML = this.tooltip;
+    this._popup.setAttribute('class', 'cps-tooltip');
+
+    const { x, y } = this._setTooltipPosition();
+
+    this._popup.style.top = y.toString() + 'px';
+    this._popup.style.left = x.toString() + 'px';
+  }
+
+  private _checkIfEnoughSpace(
+    coords: { x: number; y: number },
+    popup: HTMLDivElement
+  ) {
+    if (
+      coords.x + popup.getBoundingClientRect().width > window.innerWidth ||
+      coords.x - popup.getBoundingClientRect().width < 0
+    )
+      return false;
+
+    return true;
+  }
 
   private _setTooltipPosition() {
     switch (this.position) {
@@ -46,12 +89,14 @@ export class TooltipDirective {
             this._elementRef.nativeElement.offsetWidth / 2,
           y:
             this._elementRef.nativeElement.getBoundingClientRect().top +
-            this._elementRef.nativeElement.offsetHeight +
-            6
+            this._elementRef.nativeElement.offsetHeight
         };
       case 'left':
         return {
-          x: this._elementRef.nativeElement.getBoundingClientRect().left - 6,
+          x:
+            this._elementRef.nativeElement.getBoundingClientRect().left -
+            this._popup.getBoundingClientRect().width -
+            6,
           y:
             this._elementRef.nativeElement.getBoundingClientRect().top +
             this._elementRef.nativeElement.offsetHeight / 2
@@ -95,11 +140,11 @@ export class TooltipDirective {
   }
 
   @HostListener('click') onClick() {
-    if (this.openOn === 'click' && !this.opened) {
-      this.opened = true;
+    if (this.openOn === 'click' && !this._opened) {
+      this._opened = true;
       setTimeout(this._createTooltip, this.openDelay as number);
-    } else if (this.closeOnContentClick && this.opened) {
-      this.opened = false;
+    } else if (this.closeOnContentClick && this._opened) {
+      this._opened = false;
       setTimeout(this._destroyTooltip, this.closeDelay as number);
     }
   }
