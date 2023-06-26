c40450dff995f1397042f46504dae099e315c023
Andrei <fateeand@gmail.com>
2023-06-26 15:08:31 +0200

    api

--- projects/cps-ui-kit/src/lib/components/cps-table/cps-table.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-table/cps-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,4 +8,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cps-table.component.html',
   styleUrls: ['./cps-table.component.scss']
 })
-export class CpsTableComponent {}
+export class CpsTableComponent {
+  @Input() headers: string[] = [];
+  @Input() data: any[] = [];
+  @Input() striped = false;
+  @Input() bordered = false;
+  @Input() size: 'small' | 'default' = 'default';
+  @Input() selectable = false;
+  @Input() emptyMessage = 'No data';
+  @Input() virtualScroll = false;
+  @Input() hasToolbar = false;
+  @Input() toolbarTitle = '';
+  @Input() export = false;
+}
