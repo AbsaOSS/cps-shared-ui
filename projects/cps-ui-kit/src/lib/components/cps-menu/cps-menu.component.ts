0788ee46c113f9aafdcd5122d3cd2ae0758eaa06
Andrei <fateeand@gmail.com>
2023-07-20 18:17:49 +0200

    first draft version

--- projects/cps-ui-kit/src/lib/components/cps-menu/cps-menu.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-menu/cps-menu.component.ts
@@ -1,11 +1,502 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import {
+//   Component,
+//   EventEmitter,
+//   Input,
+//   Output,
+//   ViewChild
+// } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { OverlayPanel, OverlayPanelModule } from 'primeng/overlaypanel';
+
+// export type CpsMenuItem = {
+//   label: string;
+//   action?: (event?: any) => void;
+//   icon?: string;
+//   desc?: string;
+//   disabled?: boolean;
+// };
+
+// @Component({
+//   selector: 'cps-menu',
+//   standalone: true,
+//   imports: [CommonModule, OverlayPanelModule],
+//   templateUrl: './cps-menu.component.html',
+//   styleUrls: ['./cps-menu.component.scss']
+// })
+// export class CpsMenuComponent {
+//   @Input() header = '';
+//   @Input() items: CpsMenuItem[] = [];
+//   @Input() withArrow = false;
+//   @Input() styleClass = 'cps-menu-container';
+//   @Input() contentStyleClass = 'cps-menu-content';
+
+//   @Output() menuShown = new EventEmitter();
+//   @Output() menuHidden = new EventEmitter();
+
+//   @ViewChild('panel')
+//   panel!: OverlayPanel;
+
+//   toggle(event: any, target?: any) {
+//     this.panel.toggle(event, target);
+//   }
+
+//   show(event: any, target?: any) {
+//     this.panel.show(event, target);
+//   }
+
+//   hide() {
+//     this.panel.hide();
+//   }
+
+//   onShowPanel() {
+//     this.menuShown.emit();
+//   }
+
+//   onHidePanel() {
+//     this.menuHidden.emit();
+//   }
+// }
+
+import {
+  animate,
+  AnimationEvent,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { CommonModule, DOCUMENT, isPlatformBrowser } from '@angular/common';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  ElementRef,
+  EventEmitter,
+  Inject,
+  Input,
+  NgZone,
+  OnDestroy,
+  Output,
+  PLATFORM_ID,
+  QueryList,
+  Renderer2,
+  TemplateRef,
+  ViewEncapsulation,
+  ViewRef
+} from '@angular/core';
+import {
+  OverlayService,
+  PrimeNGConfig,
+  PrimeTemplate,
+  SharedModule
+} from 'primeng/api';
+import { ConnectedOverlayScrollHandler, DomHandler } from 'primeng/dom';
+import { ZIndexUtils } from 'primeng/utils';
+import { Subscription } from 'rxjs';
+
+type Nullable<T = void> = T | null | undefined;
+type VoidListener = () => void | null | undefined;
+
+export type CpsMenuItem = {
+  label: string;
+  action?: (event?: any) => void;
+  icon?: string;
+  desc?: string;
+  disabled?: boolean;
+};
 
 @Component({
-  selector: 'cps-menu',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
+  selector: 'cps-menu',
   templateUrl: './cps-menu.component.html',
-  styleUrls: ['./cps-menu.component.scss']
+  styleUrls: ['./cps-menu.component.scss'],
+  animations: [
+    trigger('animation', [
+      state(
+        'void',
+        style({
+          transform: 'scaleY(0.8)',
+          opacity: 0
+        })
+      ),
+      state(
+        'close',
+        style({
+          opacity: 0
+        })
+      ),
+      state(
+        'open',
+        style({
+          transform: 'translateY(0)',
+          opacity: 1
+        })
+      ),
+      transition('void => open', animate('{{showTransitionParams}}')),
+      transition('open => close', animate('{{hideTransitionParams}}'))
+    ])
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
-export class CpsMenuComponent {}
+export class CpsMenuComponent implements AfterContentInit, OnDestroy {
+  @Input() header = ''; // TODO
+  @Input() items: CpsMenuItem[] = []; // TODO
+  @Input() withArrow = false; // TODO
+  @Input() styleClass = 'cps-menu-container'; // TODO
+  @Input() contentStyleClass = 'cps-menu-content'; // TODO
+  @Input() focusOnShow = true;
+
+  @Output() menuShown = new EventEmitter();
+  @Output() menuHidden = new EventEmitter();
+
+  @ContentChildren(PrimeTemplate)
+  templates: // TODO get rid of templates, use items and header or ng-content
+  QueryList<PrimeTemplate> | undefined;
+
+  autoZIndex = true;
+  baseZIndex = 0;
+  showTransitionOptions = '.12s cubic-bezier(0, 0, 0.2, 1)';
+  hideTransitionOptions = '.1s linear';
+  dismissable = true;
+  container: Nullable<HTMLDivElement>;
+  overlayVisible = false;
+  render = false;
+  isOverlayAnimationInProgress = false;
+  selfClick = false;
+  target: any;
+  willHide: Nullable<boolean>;
+  scrollHandler: Nullable<ConnectedOverlayScrollHandler>;
+
+  documentResizeListener!: VoidListener | null;
+  overlayEventListener: Nullable<(event?: any) => void>;
+  documentClickListener!: VoidListener | null;
+
+  contentTemplate!: TemplateRef<any>;
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  destroyCallback: Nullable<Function>;
+  overlaySubscription: Subscription | undefined;
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: any,
+    public el: ElementRef,
+    public renderer: Renderer2,
+    public cd: ChangeDetectorRef,
+    private zone: NgZone,
+    public config: PrimeNGConfig,
+    public overlayService: OverlayService
+  ) {}
+
+  ngAfterContentInit() {
+    this.templates?.forEach((item) => {
+      switch (item.getType()) {
+        case 'content':
+          this.contentTemplate = item.template;
+          break;
+
+        default:
+          this.contentTemplate = item.template;
+          break;
+      }
+
+      this.cd.markForCheck();
+    });
+  }
+
+  toggle(event: any, target?: any) {
+    if (this.isOverlayAnimationInProgress) {
+      return;
+    }
+
+    if (this.overlayVisible) {
+      if (this.hasTargetChanged(event, target)) {
+        this.destroyCallback = () => {
+          this.show(null, target || event.currentTarget || event.target);
+        };
+      }
+
+      this.hide();
+    } else {
+      this.show(event, target);
+    }
+  }
+
+  show(event: any, target?: any) {
+    target && event && event.stopPropagation();
+    if (this.isOverlayAnimationInProgress) {
+      return;
+    }
+
+    this.target = target || event.currentTarget || event.target;
+    this.overlayVisible = true;
+    this.render = true;
+    this.cd.markForCheck();
+  }
+
+  hide() {
+    this.overlayVisible = false;
+    this.cd.markForCheck();
+  }
+
+  bindDocumentClickListener() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (!this.documentClickListener && this.dismissable) {
+        this.zone.runOutsideAngular(() => {
+          const documentEvent = DomHandler.isIOS() ? 'touchstart' : 'click';
+          const documentTarget: any = this.el
+            ? this.el.nativeElement.ownerDocument
+            : this.document;
+
+          this.documentClickListener = this.renderer.listen(
+            documentTarget,
+            documentEvent,
+            (event) => {
+              if (
+                !this.container?.contains(event.target) &&
+                this.target !== event.target &&
+                !this.target.contains(event.target) &&
+                !this.selfClick
+              ) {
+                this.zone.run(() => {
+                  this.hide();
+                });
+              }
+
+              this.selfClick = false;
+              this.cd.markForCheck();
+            }
+          );
+        });
+      }
+    }
+  }
+
+  unbindDocumentClickListener() {
+    if (this.documentClickListener) {
+      this.documentClickListener();
+      this.documentClickListener = null;
+      this.selfClick = false;
+    }
+  }
+
+  onOverlayClick(event: MouseEvent) {
+    this.overlayService.add({
+      originalEvent: event,
+      target: this.el.nativeElement
+    });
+
+    this.selfClick = true;
+  }
+
+  onContentClick() {
+    this.selfClick = true;
+  }
+
+  hasTargetChanged(event: any, target: any) {
+    return (
+      this.target != null &&
+      this.target !== (target || event.currentTarget || event.target)
+    );
+  }
+
+  appendContainer() {
+    this.renderer.appendChild(this.document.body, this.container);
+  }
+
+  restoreAppend() {
+    if (this.container) {
+      this.renderer.appendChild(this.el.nativeElement, this.container);
+    }
+  }
+
+  align() {
+    if (this.autoZIndex) {
+      ZIndexUtils.set(
+        'overlay',
+        this.container,
+        this.baseZIndex + this.config.zIndex.overlay
+      );
+    }
+
+    DomHandler.absolutePosition(this.container, this.target);
+
+    const containerOffset = DomHandler.getOffset(this.container);
+    const targetOffset = DomHandler.getOffset(this.target);
+    const borderRadius = this.document.defaultView
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      ?.getComputedStyle(this.container!)
+      .getPropertyValue('border-radius');
+    let arrowLeft = 0;
+
+    if (containerOffset.left < targetOffset.left) {
+      arrowLeft =
+        targetOffset.left -
+        containerOffset.left -
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        parseFloat(borderRadius!) * 2;
+    }
+    this.container?.style.setProperty('--overlayArrowLeft', `${arrowLeft}px`);
+
+    if (containerOffset.top < targetOffset.top) {
+      DomHandler.addClass(this.container, 'cps-menu-container-flipped');
+    }
+  }
+
+  onAnimationStart(event: AnimationEvent) {
+    if (event.toState === 'open') {
+      this.container = event.element;
+      this.appendContainer();
+      this.align();
+      this.bindDocumentClickListener();
+      this.bindDocumentResizeListener();
+      this.bindScrollListener();
+
+      if (this.focusOnShow) {
+        this.focus();
+      }
+
+      this.overlayEventListener = (e) => {
+        if (this.container && this.container.contains(e.target)) {
+          this.selfClick = true;
+        }
+      };
+
+      this.overlaySubscription = this.overlayService.clickObservable.subscribe(
+        this.overlayEventListener
+      );
+      this.menuShown.emit(null);
+    }
+
+    this.isOverlayAnimationInProgress = true;
+  }
+
+  onAnimationEnd(event: AnimationEvent) {
+    switch (event.toState) {
+      case 'void':
+        if (this.destroyCallback) {
+          this.destroyCallback();
+          this.destroyCallback = null;
+        }
+
+        if (this.overlaySubscription) {
+          this.overlaySubscription.unsubscribe();
+        }
+        break;
+
+      case 'close':
+        if (this.autoZIndex) {
+          ZIndexUtils.clear(this.container);
+        }
+
+        if (this.overlaySubscription) {
+          this.overlaySubscription.unsubscribe();
+        }
+
+        this.onContainerDestroy();
+        this.menuHidden.emit({});
+        this.render = false;
+        break;
+    }
+
+    this.isOverlayAnimationInProgress = false;
+  }
+
+  focus() {
+    const focusable = DomHandler.findSingle(this.container, '[autofocus]');
+    if (focusable) {
+      this.zone.runOutsideAngular(() => {
+        setTimeout(() => focusable.focus(), 5);
+      });
+    }
+  }
+
+  onWindowResize() {
+    if (this.overlayVisible && !DomHandler.isTouchDevice()) {
+      this.hide();
+    }
+  }
+
+  bindDocumentResizeListener() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (!this.documentResizeListener) {
+        const window = this.document.defaultView as Window;
+        this.documentResizeListener = this.renderer.listen(
+          window,
+          'resize',
+          this.onWindowResize.bind(this)
+        );
+      }
+    }
+  }
+
+  unbindDocumentResizeListener() {
+    if (this.documentResizeListener) {
+      this.documentResizeListener();
+      this.documentResizeListener = null;
+    }
+  }
+
+  bindScrollListener() {
+    if (isPlatformBrowser(this.platformId)) {
+      if (!this.scrollHandler) {
+        this.scrollHandler = new ConnectedOverlayScrollHandler(
+          this.target,
+          () => {
+            if (this.overlayVisible) {
+              this.hide();
+            }
+          }
+        );
+      }
+
+      this.scrollHandler.bindScrollListener();
+    }
+  }
+
+  unbindScrollListener() {
+    if (this.scrollHandler) {
+      this.scrollHandler.unbindScrollListener();
+    }
+  }
+
+  onContainerDestroy() {
+    if (!(this.cd as ViewRef).destroyed) {
+      this.target = null;
+    }
+
+    this.unbindDocumentClickListener();
+    this.unbindDocumentResizeListener();
+    this.unbindScrollListener();
+  }
+
+  ngOnDestroy() {
+    if (this.scrollHandler) {
+      this.scrollHandler.destroy();
+      this.scrollHandler = null;
+    }
+
+    if (this.container && this.autoZIndex) {
+      ZIndexUtils.clear(this.container);
+    }
+
+    if (!(this.cd as ViewRef).destroyed) {
+      this.target = null;
+    }
+
+    this.destroyCallback = null;
+    if (this.container) {
+      this.restoreAppend();
+      this.onContainerDestroy();
+    }
+
+    if (this.overlaySubscription) {
+      this.overlaySubscription.unsubscribe();
+    }
+  }
+}
