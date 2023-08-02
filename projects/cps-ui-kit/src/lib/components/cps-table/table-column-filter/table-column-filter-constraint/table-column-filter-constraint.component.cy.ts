a3f4ea6954e5b279c05edf436235c264a2361645
Andrei <fateeand@gmail.com>
2023-08-02 15:03:17 +0200

    remove unnecessary tests

--- projects/cps-ui-kit/src/lib/components/cps-table/table-column-filter/table-column-filter-constraint/table-column-filter-constraint.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { TableColumnFilterConstraintComponent } from './table-column-filter-constraint.component';
-
-describe('TableColumnFilterConstraintComponent', () => {
-  it('should mount', () => {
-    cy.mount(TableColumnFilterConstraintComponent);
-  });
-});
--- projects/cps-ui-kit/src/lib/components/cps-table/table-column-filter/table-column-filter.component.cy.ts
+++
@@ -1,7 +0,0 @@
-import { TableColumnFilterComponent } from './table-column-filter.component';
-
-describe('TableColumnFilterComponent', () => {
-  it('should mount', () => {
-    cy.mount(TableColumnFilterComponent);
-  });
-});
