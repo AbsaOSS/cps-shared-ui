4e32dde14cbd77efde3628d9500f48069473ebf8
Andrei <fateeand@gmail.com>
2023-10-24 22:02:00 +0200

    fix value update in tree-based inputs with ngModel

--- projects/cps-ui-kit/src/lib/base_components/base-tree-dropdown.component.ts
+++ projects/cps-ui-kit/src/lib/base_components/base-tree-dropdown.component.ts
@@ -218,12 +218,15 @@ export class BaseTreeDropdownComponent
     this.onTouched = fn;
   }
 
-  writeValue(value: any) {
+  writeValue(value: any, internal = false) {
     this.value = value;
+    if (!internal && value !== null) {
+      this.treeSelection = this._valueToTreeSelection(this.value);
+    }
   }
 
   updateValue(value: any): void {
-    this.writeValue(value);
+    this.writeValue(value, true);
     this.onChange(value);
     this.valueChanged.emit(value);
   }
