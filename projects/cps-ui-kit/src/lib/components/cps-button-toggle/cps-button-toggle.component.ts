233b8b5840b69e5322b7114b0f39baff5d71a030
Andrei Fateev <104006376+fateeand@users.noreply.github.com>
2023-11-11 19:52:28 +0100

    Added column filter as btn toggle + multiple enhancements, bugfixes (#248)
    * Added filter as button toggle option
    
    * fixes
    
    * calc button toggles width when not visible
    
    * more to prev
    
    * expose hideFilter method
    
    * don't close multi autocomplete filter dropdown

--- projects/cps-ui-kit/src/lib/components/cps-button-toggle/cps-button-toggle.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-button-toggle/cps-button-toggle.component.ts
@@ -1,17 +1,13 @@
 import { CommonModule } from '@angular/common';
 import {
-  AfterViewInit,
   Component,
-  ElementRef,
   EventEmitter,
   Input,
   OnInit,
   Optional,
   Output,
-  QueryList,
   Renderer2,
-  Self,
-  ViewChildren
+  Self
 } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
 import { isEqual } from 'lodash-es';
@@ -23,7 +19,7 @@ import {
   TooltipPosition
 } from '../../directives/cps-tooltip.directive';
 
-export type BtnToggleOption = {
+export type CpsButtonToggleOption = {
   value: any;
   label?: string;
   icon?: string;
@@ -45,11 +41,9 @@ export type BtnToggleOption = {
   templateUrl: './cps-button-toggle.component.html',
   styleUrls: ['./cps-button-toggle.component.scss']
 })
-export class CpsButtonToggleComponent
-  implements ControlValueAccessor, OnInit, AfterViewInit
-{
+export class CpsButtonToggleComponent implements ControlValueAccessor, OnInit {
   @Input() label = '';
-  @Input() options = [] as BtnToggleOption[];
+  @Input() options = [] as CpsButtonToggleOption[];
   @Input() multiple = false;
   @Input() disabled = false;
   @Input() mandatory = true; // at least one of the options is mandatory
@@ -74,7 +68,7 @@ export class CpsButtonToggleComponent
 
   @Output() valueChanged = new EventEmitter<any>();
 
-  @ViewChildren('optionContent') optionsList!: QueryList<ElementRef>;
+  largestButtonWidth = 0;
 
   constructor(
     @Self() @Optional() private _control: NgControl,
@@ -89,9 +83,6 @@ export class CpsButtonToggleComponent
     if (this.multiple && !this._value) {
       this._value = [];
     }
-  }
-
-  ngAfterViewInit(): void {
     this._setEqualWidths();
   }
 
@@ -156,25 +147,38 @@ export class CpsButtonToggleComponent
   private _setEqualWidths() {
     if (!this.equalWidths) return;
 
-    const contentOptions =
-      this.optionsList
-        ?.toArray()
-        ?.filter((opt) => opt?.nativeElement)
-        ?.map((opt) => opt.nativeElement) || [];
+    const hiddenSpan = this.renderer.createElement('span');
+    this.renderer.setStyle(hiddenSpan, 'visibility', 'hidden');
+    this.renderer.setStyle(hiddenSpan, 'position', 'absolute');
+    this.renderer.setStyle(hiddenSpan, 'left', '-9999px');
+    this.renderer.setStyle(hiddenSpan, 'font-size', '16px');
+    this.renderer.setStyle(hiddenSpan, 'letter-spacing', '0.05em');
+    this.renderer.setStyle(
+      hiddenSpan,
+      'font-family',
+      '"Source Sans Pro", sans-serif'
+    );
 
-    let largestButtonWidth = 0;
-    contentOptions.forEach((elem) => {
-      const buttonWidth = elem.offsetWidth || 0;
-      if (buttonWidth > largestButtonWidth) {
-        largestButtonWidth = buttonWidth;
+    this.renderer.appendChild(document.body, hiddenSpan);
+
+    this.largestButtonWidth = 0;
+    this.options.forEach((opt) => {
+      const text = this.renderer.createText(opt.label || '');
+      this.renderer.appendChild(hiddenSpan, text);
+
+      let width = hiddenSpan.offsetWidth || 0;
+      width += 26;
+      if (opt.icon) {
+        width += 16;
+        if (opt.label) width += 8;
       }
+      if (width > this.largestButtonWidth) {
+        this.largestButtonWidth = width;
+      }
+      this.renderer.removeChild(hiddenSpan, text);
     });
 
-    if (largestButtonWidth > 0) {
-      contentOptions.forEach((elem) => {
-        this.renderer.setStyle(elem, 'min-width', largestButtonWidth + 'px');
-      });
-    }
+    this.renderer.removeChild(document.body, hiddenSpan);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
--- projects/cps-ui-kit/src/lib/components/cps-table/table-column-filter/table-column-filter-constraint/table-column-filter-constraint.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-table/table-column-filter/table-column-filter-constraint/table-column-filter-constraint.component.ts
@@ -1,4 +1,10 @@
-import { Component, Input, OnChanges, Optional } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Optional,
+  ViewChild
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FilterMetadata } from 'primeng/api';
@@ -7,10 +13,14 @@ import { CpsInputComponent } from '../../../cps-input/cps-input.component';
 import { CpsDatepickerComponent } from '../../../cps-datepicker/cps-datepicker.component';
 import { CpsAutocompleteComponent } from '../../../cps-autocomplete/cps-autocomplete.component';
 import {
-  BtnToggleOption,
+  CpsButtonToggleOption,
   CpsButtonToggleComponent
 } from '../../../cps-button-toggle/cps-button-toggle.component';
 import { TreeTable } from 'primeng/treetable';
+import {
+  CpsColumnFilterCategoryOption,
+  CpsColumnFilterType
+} from '../../cps-column-filter-types';
 
 @Component({
   selector: 'table-column-filter-constraint',
@@ -27,21 +37,30 @@ import { TreeTable } from 'primeng/treetable';
   styleUrls: ['./table-column-filter-constraint.component.scss']
 })
 export class TableColumnFilterConstraintComponent implements OnChanges {
-  @Input() type: string | undefined;
+  @Input() type: CpsColumnFilterType = 'text';
   @Input() field: string | undefined;
   @Input() filterConstraint: FilterMetadata | undefined;
-  @Input() categoryOptions: string[] = [];
+  @Input() categoryOptions: CpsColumnFilterCategoryOption[] | string[] = [];
+  @Input() asButtonToggle = false; // for category type only
+  @Input() singleSelection = false; // for category type only
   @Input() placeholder = '';
   @Input() hasApplyButton = true;
+
+  @ViewChild('categoryAutocompleteComponent')
+  categoryAutocompleteComponent?: CpsAutocompleteComponent;
 
   booleanOptions = [
     { label: 'True', value: 'true' },
     { label: 'False', value: 'false' }
-  ] as BtnToggleOption[];
+  ] as CpsButtonToggleOption[];
 
-  categories: { label: string; value: string }[] = [];
+  categories: CpsColumnFilterCategoryOption[] = [];
 
   _tableInstance: Table | TreeTable;
+
+  get isCategoryDropdownOpened() {
+    return this.categoryAutocompleteComponent?.isOpened || false;
+  }
 
   constructor(@Optional() public dt: Table, @Optional() public tt: TreeTable) {
     this._tableInstance = dt || tt;
@@ -54,10 +73,15 @@ export class TableColumnFilterConstraintComponent implements OnChanges {
   private _updateCategories() {
     if (this.type !== 'category') return;
     if (this.categoryOptions.length > 0) {
-      this.categories = this.categoryOptions.map((o) => ({
-        label: o,
-        value: o
-      }));
+      if (typeof this.categoryOptions[0] === 'string') {
+        this.categories = (this.categoryOptions as string[]).map((o) => ({
+          label: o,
+          value: o
+        }));
+      } else {
+        this.categories = this
+          .categoryOptions as CpsColumnFilterCategoryOption[];
+      }
     } else {
       let cats = [];
       if (this._tableInstance instanceof Table) {
