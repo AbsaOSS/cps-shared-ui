9ef00c905ea8310527933e6681af1ddc95466c85
Andrei <fateeand@gmail.com>
2024-01-15 17:56:59 +0100

    multiple unsort support

--- projects/cps-ui-kit/src/lib/components/cps-table/directives/internal/table-unsort.directive.ts
+++ projects/cps-ui-kit/src/lib/components/cps-table/directives/internal/table-unsort.directive.ts
@@ -43,10 +43,8 @@ export class TableUnsortDirective {
             pTable.multiSortMeta.push(sortMeta);
           }
           if (pTable._multiSortMeta?.length === 0) {
-            pTable.multiSortMeta.push({
-              field: '_defaultSortOrder',
-              order: pTable.defaultSortOrder
-            });
+            (pTable.multiSortMeta as any) = null;
+            this.resetSorting = true;
             resetIndex = true;
           }
         } else {
@@ -105,37 +103,7 @@ export class TableUnsortDirective {
               order
             });
           } else {
-            // restore default sort order
-            if (this.resetSorting && this.sortIndices.length > 0) {
-              pTable._value = this.sortIndices.map(
-                (index) => pTable._value[index]
-              );
-              this.resetDefaultSortOrder();
-            } else {
-              const toSort: any[] = [];
-              if (this.sortIndices.length < 1) {
-                this.sortIndices = Array.from(
-                  { length: pTable.value.length },
-                  (_, index) => index
-                );
-              }
-              for (let i = 0; i < pTable.value.length; i++) {
-                toSort.push([pTable.value[i], this.sortIndices.indexOf(i)]);
-              }
-
-              toSort.sort((data1, data2) => {
-                const value1 = ObjectUtils.resolveFieldData(data1[0], field);
-                const value2 = ObjectUtils.resolveFieldData(data2[0], field);
-                return pTable.compareValuesOnSort(value1, value2, order);
-              });
-
-              for (let i = 0; i < toSort.length; i++) {
-                this.sortIndices[toSort[i][1]] = i;
-                pTable.value[i] = toSort[i][0];
-              }
-            }
-
-            pTable._value = [...pTable.value];
+            this._sortData(field, order);
           }
 
           if (pTable.hasFilter()) {
@@ -152,9 +120,186 @@ export class TableUnsortDirective {
         pTable.tableService.onSort(sortMeta);
       }
     };
+
+    pTable.sortMultiple = () => {
+      if (pTable.groupRowsBy) {
+        if (!pTable._multiSortMeta)
+          pTable._multiSortMeta = [pTable.getGroupRowsMeta()];
+        else if (pTable.multiSortMeta[0].field !== pTable.groupRowsBy)
+          pTable._multiSortMeta = [
+            pTable.getGroupRowsMeta(),
+            ...pTable._multiSortMeta
+          ];
+      }
+
+      if (pTable.multiSortMeta || this.resetSorting) {
+        if (pTable.lazy) {
+          pTable.onLazyLoad.emit(pTable.createLazyLoadMetadata());
+        } else if (pTable.value) {
+          if (pTable.customSort) {
+            pTable.sortFunction.emit({
+              data: pTable.value,
+              mode: pTable.sortMode,
+              multiSortMeta: pTable.multiSortMeta
+            });
+          } else {
+            this._sortData();
+          }
+
+          if (pTable.hasFilter()) {
+            pTable._filter();
+          }
+        }
+
+        pTable.onSort.emit({
+          multisortmeta: pTable.multiSortMeta
+        });
+        pTable.tableService.onSort(pTable.multiSortMeta);
+      }
+    };
   }
 
   resetDefaultSortOrder() {
     this.sortIndices = [];
   }
+
+  private _sortData(field?: any, order?: any) {
+    // restore default sort order
+    if (this.resetSorting && this.sortIndices.length > 0) {
+      this.pTable._value = this.sortIndices.map(
+        (index) => this.pTable._value[index]
+      );
+      this.resetDefaultSortOrder();
+    } else {
+      const toSort: any[] = [];
+      if (this.sortIndices.length < 1) {
+        this.sortIndices = Array.from(
+          { length: this.pTable.value.length },
+          (_, index) => index
+        );
+      }
+
+      for (let i = 0; i < this.pTable.value.length; i++) {
+        toSort.push([this.pTable.value[i], this.sortIndices.indexOf(i)]);
+      }
+
+      if (this.pTable.sortMode === 'single') {
+        toSort.sort((data1, data2) => {
+          const value1 = ObjectUtils.resolveFieldData(data1[0], field);
+          const value2 = ObjectUtils.resolveFieldData(data2[0], field);
+          return this.pTable.compareValuesOnSort(value1, value2, order);
+        });
+      } else {
+        toSort.sort((data1, data2) => {
+          return this._multisortField(data1, data2, 0);
+        });
+      }
+
+      for (let i = 0; i < toSort.length; i++) {
+        this.sortIndices[toSort[i][1]] = i;
+        this.pTable.value[i] = toSort[i][0];
+      }
+    }
+
+    this.pTable._value = [...this.pTable.value];
+  }
+
+  private _multisortField(data1: any, data2: any, index: number): number {
+    const value1 = ObjectUtils.resolveFieldData(
+      data1[0],
+      this.pTable.multiSortMeta[index].field
+    );
+    const value2 = ObjectUtils.resolveFieldData(
+      data2[0],
+      this.pTable.multiSortMeta[index].field
+    );
+    if (ObjectUtils.compare(value1, value2, this.pTable.filterLocale) === 0) {
+      return this.pTable.multiSortMeta.length - 1 > index
+        ? this._multisortField(data1, data2, index + 1)
+        : 0;
+    }
+    return this.pTable.compareValuesOnSort(
+      value1,
+      value2,
+      this.pTable.multiSortMeta[index].order
+    );
+  }
 }
+
+// import { Directive, Host, Self, Optional } from '@angular/core';
+// import { Table } from 'primeng/table';
+
+// @Directive({
+//   standalone: true,
+//   selector: '[tWithUnsort]'
+// })
+// export class TableUnsortDirective {
+//   defaultSortOrderInitialized = false;
+
+//   constructor(@Host() @Self() @Optional() public pTable: Table) {
+//     pTable.tableService.valueSource$.subscribe((val: any[]) => {
+//       if (val != null && val.length > 0 && !this.defaultSortOrderInitialized) {
+//         let i = 0;
+//         val.forEach((v) => {
+//           v._defaultSortOrder = i++;
+//         });
+//         this.defaultSortOrderInitialized = true;
+//       }
+//     });
+
+//     pTable.sort = (event: any) => {
+//       if (pTable.sortMode === 'single') {
+//         if (pTable.sortField === event.field && pTable.sortOrder === -1) {
+//           event.field = '_defaultSortOrder';
+//         }
+
+//         pTable._sortOrder =
+//           pTable.sortField === event.field
+//             ? pTable.sortOrder * -1
+//             : pTable.defaultSortOrder;
+//         pTable._sortField = event.field;
+//         pTable.sortSingle();
+//       }
+//       if (pTable.sortMode === 'multiple') {
+//         let resetIndex = false;
+//         const sortMeta = pTable.getSortMeta(event.field);
+
+//         if (sortMeta) {
+//           for (let i = 0; i < pTable._multiSortMeta.length; i++) {
+//             if (pTable._multiSortMeta[i].field === sortMeta.field) {
+//               pTable._multiSortMeta.splice(i, 1);
+//             }
+//           }
+//           if (sortMeta.order === 1) {
+//             sortMeta.order = sortMeta.order * -1;
+//             pTable.multiSortMeta.push(sortMeta);
+//           }
+//           if (pTable._multiSortMeta?.length === 0) {
+//             pTable.multiSortMeta.push({
+//               field: '_defaultSortOrder',
+//               order: pTable.defaultSortOrder
+//             });
+//             resetIndex = true;
+//           }
+//         } else {
+//           if (!pTable.multiSortMeta) {
+//             pTable._multiSortMeta = [];
+//           }
+//           pTable.multiSortMeta.push({
+//             field: event.field,
+//             order: pTable.defaultSortOrder
+//           });
+//         }
+
+//         pTable.sortMultiple();
+//         if (resetIndex) pTable._multiSortMeta = [];
+//       }
+
+//       if (pTable.isStateful()) {
+//         pTable.saveState();
+//       }
+
+//       pTable.anchorRowIndex = 0;
+//     };
+//   }
+// }
