05b28d405e3a1f58aa375ff3733ede0d278a1865
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-08-27 16:11:46 +0200

    Fix linting

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,7 +1,5 @@
 import 'jest-preset-angular/setup-jest';
 
-jest.mock("projects/cps-ui-kit/package.json", () => ({ version: '1.0.0' }), {virtual: true});
-
 window.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
--- projects/composition/src/app/app.component.spec.ts
+++ projects/composition/src/app/app.component.spec.ts
@@ -4,15 +4,15 @@ import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { CpsIconComponent } from 'cps-ui-kit';
 import { NavigationSidebarComponent } from './components/navigation-sidebar/navigation-sidebar.component';
 
+jest.mock('projects/cps-ui-kit/package.json', () => ({ version: '1.0.0' }), {
+  virtual: true
+});
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [
-        NavigationSidebarComponent,
-        CpsIconComponent,
-        RouterOutlet
-      ],
+      imports: [NavigationSidebarComponent, CpsIconComponent, RouterOutlet],
       providers: [{ provide: ActivatedRoute, useValue: {} }]
     }).compileComponents();
   });
