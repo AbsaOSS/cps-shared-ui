d5b366ecd3219cfba4bd4f671dfc95159061360c
Lukáš Matta <lukasmatta@users.noreply.github.com>
2025-06-23 17:44:07 +0200

    Replace legacy xlsx dependency (#433)
    * Add test for xlsx table export verification
    
    * Replace xlsx with @e965/xlsx

---
+++ cypress/e2e/cps-table.cy.ts
@@ -0,0 +1,25 @@
+import path = require('path');
+
+describe('cps-table page', () => {
+  describe('export to xlsx', () => {
+    beforeEach(() => {
+      cy.visit('/table');
+    });
+
+    it('should properly download valid xlsx', () => {
+      cy.contains('Table 6').click({ force: true });
+      cy.get('.cps-table-tbar-export-btn cps-icon').click();
+      cy.contains('XLSX').click();
+
+      const downloadsFolder = Cypress.config('downloadsFolder');
+      const downloadedFilePath = path.join(downloadsFolder, 'table_6.xlsx');
+      cy.readFile(downloadedFilePath).should('exist');
+
+      cy.fixture('table_6_fixture.xlsx').then((fixtureContent) => {
+        cy.readFile(downloadedFilePath).then((downloadedFileContent) => {
+          expect(downloadedFileContent).equals(fixtureContent);
+        });
+      });
+    });
+  });
+});
