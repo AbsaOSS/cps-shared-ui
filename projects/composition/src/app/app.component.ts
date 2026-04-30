76dac7152bbfb819209b7b6974e4eee69ec1e394
Lukas Matta <lukasmatta@users.noreply.github.com>
2026-04-30 11:58:31 +0200

    Add support for dark-mode

--- projects/composition/src/app/app.component.ts
+++ projects/composition/src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component, effect, inject, PLATFORM_ID } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import packageJson from 'projects/cps-ui-kit/package.json';
 import { filter, map } from 'rxjs/operators';
+import { CpsThemeService } from 'cps-ui-kit';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,8 @@ import { filter, map } from 'rxjs/operators';
 })
 export class AppComponent {
   private readonly _platformId = inject(PLATFORM_ID);
+  private readonly _document = inject(DOCUMENT);
+  private readonly _themeService = inject(CpsThemeService);
 
   componentTitle = '';
 
@@ -31,6 +34,23 @@ export class AppComponent {
         new URLSearchParams(window.location.search).get('experimental') ===
         'true';
     }
+
+    effect(() => {
+      const theme = this._themeService.isDark()
+        ? 'atom-one-dark'
+        : 'atom-one-light';
+      let link = this._document.getElementById(
+        'hljs-theme'
+      ) as HTMLLinkElement | null;
+      if (!link) {
+        link = this._document.createElement('link');
+        link.id = 'hljs-theme';
+        link.rel = 'stylesheet';
+        this._document.head.appendChild(link);
+      }
+      link.href = `hljs-themes/${theme}.min.css`;
+    });
+
     this._router.events
       .pipe(
         filter((event) => event instanceof NavigationEnd),
