8f2b06cf745a31fdedd2c21752dcfcf3c5106fc9
Andrei Fateev <104006376+fateeand@users.noreply.github.com>
2026-05-19 11:49:42 +0200

    Standardize vscode settings (#627)
    * scaffolding
    
    * remove cucumber
    
    * setup jest
    
    * change order
    
    * address feedback from copilot
    
    * update script
    
    * remove script

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,3 +1,12 @@
+import { getTestBed } from '@angular/core/testing';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+
+// Guard against double-initialisation.
+// @angular-builders/jest v20+ injects its own setupZoneTestEnv() before this file runs.
+if (!getTestBed().platform) {
+  setupZoneTestEnv();
+}
+
 window.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
