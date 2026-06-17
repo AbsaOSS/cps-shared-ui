e9d6438e8d6d3ec8eeaba08722d2853d38b91931
Andrei Fateev <104006376+fateeand@users.noreply.github.com>
2026-06-17 11:25:00 +0200

    feat: fix a11y issues in progress circular component (#667)

--- projects/cps-ui-kit/src/lib/components/cps-progress-circular/cps-progress-circular.component.ts
+++ projects/cps-ui-kit/src/lib/components/cps-progress-circular/cps-progress-circular.component.ts
@@ -1,5 +1,13 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  HostAttributeToken,
+  Input,
+  OnInit,
+  OnChanges,
+  inject,
+  type SimpleChanges
+} from '@angular/core';
 import { convertSize } from '../../utils/internal/size-utils';
 import { getCSSColor } from '../../utils/colors-utils';
 
@@ -11,20 +19,24 @@ import { getCSSColor } from '../../utils/colors-utils';
   imports: [],
   selector: 'cps-progress-circular',
   templateUrl: './cps-progress-circular.component.html',
-  styleUrls: ['./cps-progress-circular.component.scss']
+  styleUrls: ['./cps-progress-circular.component.scss'],
+  host: {
+    role: 'progressbar',
+    '[attr.aria-label]': 'computedAriaLabel'
+  }
 })
-export class CpsProgressCircularComponent implements OnInit {
+export class CpsProgressCircularComponent implements OnInit, OnChanges {
   /**
    * Diameter of the progress bar, of type number denoting pixels or string.
    * @group Props
    */
-  @Input() diameter: number | string = 40;
+  @Input() diameter: number | string = '2.5rem';
 
   /**
    * Thickness of the progress bar, of type number denoting pixels or string.
    * @group Props
    */
-  @Input() strokeWidth: number | string = 4;
+  @Input() strokeWidth: number | string = '0.25rem';
 
   /**
    * Color of the progress bar.
@@ -32,13 +44,49 @@ export class CpsProgressCircularComponent implements OnInit {
    */
   @Input() color = 'calm';
 
-  // eslint-disable-next-line no-useless-constructor
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  /**
+   * Accessible label announced by screen readers to describe what is loading.
+   * Falls back to "Loading" when empty value is provided.
+   * @group Props
+   * @default Loading
+   */
+  @Input() ariaLabel = '';
+
+  private readonly _document = inject(DOCUMENT);
+  private readonly _staticAriaLabel = inject(
+    new HostAttributeToken('aria-label'),
+    { optional: true }
+  );
+
+  cvtDiameter = '';
+  cvtStrokeWidth = '';
+  cvtColor = '';
+  computedAriaLabel = '';
 
   ngOnInit(): void {
-    this.diameter = convertSize(this.diameter);
-    this.strokeWidth = convertSize(this.strokeWidth);
+    this.cvtDiameter = convertSize(this.diameter);
+    this.cvtStrokeWidth = convertSize(this.strokeWidth);
+    this.cvtColor = getCSSColor(this.color, this._document);
+    this._updateAriaLabel();
+  }
 
-    this.color = getCSSColor(this.color, this.document);
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.ariaLabel) {
+      this._updateAriaLabel();
+    }
+    if (changes.diameter) {
+      this.cvtDiameter = convertSize(this.diameter);
+    }
+    if (changes.strokeWidth) {
+      this.cvtStrokeWidth = convertSize(this.strokeWidth);
+    }
+    if (changes.color) {
+      this.cvtColor = getCSSColor(this.color, this._document);
+    }
+  }
+
+  private _updateAriaLabel(): void {
+    this.computedAriaLabel =
+      this.ariaLabel?.trim() || this._staticAriaLabel?.trim() || 'Loading';
   }
 }
