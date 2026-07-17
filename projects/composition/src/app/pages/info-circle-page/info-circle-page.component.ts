dc023befaa399ecf81332ddc59d0555197709afc
Andrei Fateev <104006376+fateeand@users.noreply.github.com>
2026-07-17 12:27:21 +0200

    docs: add code examples for Info Circle, Input, Loader (#750)

--- projects/composition/src/app/pages/info-circle-page/info-circle-page.component.ts
+++ projects/composition/src/app/pages/info-circle-page/info-circle-page.component.ts
@@ -4,14 +4,20 @@ import { CpsInfoCircleComponent } from 'cps-ui-kit';
 
 import ComponentData from '../../api-data/cps-info-circle.json';
 import { ComponentDocsViewerComponent } from '../../components/component-docs-viewer/component-docs-viewer.component';
+import { CodeExampleComponent } from '../../components/code-example/code-example.component';
+import { infoCircleExamples } from './info-circle-page.examples';
 
 @Component({
   selector: 'app-info-circle-page',
-  imports: [CpsInfoCircleComponent, ComponentDocsViewerComponent],
+  imports: [
+    CpsInfoCircleComponent,
+    ComponentDocsViewerComponent,
+    CodeExampleComponent
+  ],
   templateUrl: './info-circle-page.component.html',
-  styleUrls: ['./info-circle-page.component.scss'],
   host: { class: 'composition-page' }
 })
 export class InfoCirclePageComponent {
   componentData = ComponentData;
+  readonly examples = infoCircleExamples;
 }
