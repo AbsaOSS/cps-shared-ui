035f20f5ae43de55b785f16d72e453223073d227
Andrei Fateev <fateeand@gmail.com>
2026-08-05 14:24:40 +0200

    test: add Playwright coverage and test ids for CpsPaginatorComponent

--- projects/composition/src/app/pages/paginator-page/paginator-page.component.ts
+++ projects/composition/src/app/pages/paginator-page/paginator-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { CpsPaginatorComponent } from 'cps-ui-kit';
+import { FormsModule } from '@angular/forms';
+import { CpsCheckboxComponent, CpsPaginatorComponent } from 'cps-ui-kit';
 import { ComponentDocsViewerComponent } from '../../components/component-docs-viewer/component-docs-viewer.component';
 import { CodeExampleComponent } from '../../components/code-example/code-example.component';
 import ComponentData from '../../api-data/cps-paginator.json';
@@ -8,7 +9,9 @@ import { paginatorExamples } from './paginator-page.examples';
 @Component({
   selector: 'app-paginator-page',
   imports: [
+    FormsModule,
     CpsPaginatorComponent,
+    CpsCheckboxComponent,
     ComponentDocsViewerComponent,
     CodeExampleComponent
   ],
@@ -19,4 +22,13 @@ import { paginatorExamples } from './paginator-page.examples';
 export class PaginatorPageComponent {
   componentData = ComponentData;
   readonly examples = paginatorExamples;
+
+  first = 0;
+  rows = 10;
+  manyRecords = false;
+
+  onPageChanged(event: { first: number; rows: number }) {
+    this.first = event.first;
+    this.rows = event.rows;
+  }
 }
