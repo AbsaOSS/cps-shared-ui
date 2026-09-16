6235a120c76d58ad9f3abbea916ac8619d285818
Lukas Matta <lukasmatta@users.noreply.github.com>
2026-09-16 11:12:33 +0200

    Make incomplete scripts os agnostic

--- playwright/print-a11y-warnings.js
+++ playwright/print-a11y-warnings.js
@@ -4,14 +4,6 @@
 // results) - lets you find those without opening the HTML report.
 const fs = require('fs');
 
-const reportPath = process.argv[2];
-if (!reportPath) {
-  console.error('Usage: node playwright/print-a11y-warnings.js <report.json>');
-  process.exit(1);
-}
-
-const report = JSON.parse(fs.readFileSync(reportPath, 'utf8'));
-
 function* walkSpecs(suites) {
   for (const suite of suites) {
     yield* (suite.specs ?? []).map((spec) => spec);
@@ -19,23 +11,40 @@ function* walkSpecs(suites) {
   }
 }
 
-let count = 0;
-for (const spec of walkSpecs(report.suites ?? [])) {
-  for (const test of spec.tests ?? []) {
-    const warnings = (test.annotations ?? []).filter(
-      (a) => a.type === 'warning'
-    );
-    if (warnings.length === 0) continue;
-    count++;
-    console.log(
-      `\n${spec.file}:${spec.line} - ${spec.title} [${test.projectName}]`
-    );
-    for (const w of warnings) console.log(`  ${w.description}`);
+// Exported so run-a11y-incomplete.js can reuse it on an in-memory report.
+function printWarnings(report) {
+  let count = 0;
+  for (const spec of walkSpecs(report.suites ?? [])) {
+    for (const test of spec.tests ?? []) {
+      const warnings = (test.annotations ?? []).filter(
+        (a) => a.type === 'warning'
+      );
+      if (warnings.length === 0) continue;
+      count++;
+      console.log(
+        `\n${spec.file}:${spec.line} - ${spec.title} [${test.projectName}]`
+      );
+      for (const w of warnings) console.log(`  ${w.description}`);
+    }
   }
+
+  console.log(
+    count === 0
+      ? '\nNo tests with accessibility warnings found.'
+      : `\n${count} test(s) with accessibility warnings.`
+  );
+  return count;
 }
 
-console.log(
-  count === 0
-    ? '\nNo tests with accessibility warnings found.'
-    : `\n${count} test(s) with accessibility warnings.`
-);
+if (require.main === module) {
+  const reportPath = process.argv[2];
+  if (!reportPath) {
+    console.error(
+      'Usage: node playwright/print-a11y-warnings.js <report.json>'
+    );
+    process.exit(1);
+  }
+  printWarnings(JSON.parse(fs.readFileSync(reportPath, 'utf8')));
+}
+
+module.exports = { printWarnings };
